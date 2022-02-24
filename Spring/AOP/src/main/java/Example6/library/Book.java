d05b0f7b53dc2a09d0d519e1ec249fc41da866e0
saumlaki <int_o@mail.ru>
2022-02-24 10:24:40 +0300

    Around advice

---
+++ Spring/AOP/src/main/java/Example6/library/Book.java
@@ -0,0 +1,23 @@
+package Example6.library;
+
+/**Класс описывает книгу*/
+public class Book {
+    String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
